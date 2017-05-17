05e12b52bd785751611ddb4e723c02be0a84b780
wrydz <wrydzfiying@163.com>
2017-05-17 21:49:50 +0800

    l

---
+++ layers/wrydz/config.el
@@ -0,0 +1,6 @@
+(setq org-startup-indented t)
+
+;; use apsell as ispell backend
+(setq-default ispell-program-name "aspell")
+;; use American English as ispell default dictionary
+(ispell-change-dictionary "american" t)
