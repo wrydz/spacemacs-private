d263a3e68e9f25545d36291aa6b5660fdd049736
wrydz <wrydzfiying@163.com>
2017-05-17 21:41:14 +0800

    修改目录名

--- layer/wrydz/config.el
+++
@@ -1,6 +0,0 @@
-(setq org-startup-indented t)
-
-;; use apsell as ispell backend
-(setq-default ispell-program-name "aspell")
-;; use American English as ispell default dictionary
-(ispell-change-dictionary "american" t)
