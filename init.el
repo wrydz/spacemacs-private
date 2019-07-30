3a19f1f5d4eac4af712dec269a61efd883f7bd8f
wrydz <wrydzfiying@163.com>
2019-07-30 18:16:23 +0800

    update

--- init.el
+++ init.el
@@ -483,10 +483,10 @@ This function is called immediately after `dotspacemacs/init', before layer
 configuration.
 It is mostly for variables that should be set before packages are loaded.
 If you are unsure, try setting them in `dotspacemacs/user-config' first."
-   ;; (setq configuration-layer-elpa-archives
-   ;;       '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
-   ;;         ("org-cn"   . "http://elpa.emacs-china.org/org/")
-   ;;         ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
+  (setq configuration-layer-elpa-archives
+        '(("melpa-cn" . "http://elpa.emacs-china.org/melpa/")
+          ("org-cn"   . "http://elpa.emacs-china.org/org/")
+          ("gnu-cn"   . "http://elpa.emacs-china.org/gnu/")))
   (add-to-list 'configuration-layer-elpa-archives '("melpa-stable" . "stable.melpa.org/packages/"))
   ;; (add-to-list 'package-pinned-packages '(ensime . "melpa-stable"))
   )
@@ -504,29 +504,9 @@ This function is called at the very end of Spacemacs startup, after layer
 configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
-  (setq-default flycheck-scalastylerc "/usr/local/etc/scalastyle_config.xml")
+  ;; (setq-default flycheck-scalastylerc "/usr/local/etc/scalastyle_config.xml")
   (setq org-reveal-note-key-char nil)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
 ;; auto-generate custom variable definitions.
-(defun dotspacemacs/emacs-custom-settings ()
-  "Emacs custom settings.
-This is an auto-generated function, do not modify its content directly, use
-Emacs customize menu instead.
-This function is called at the very end of Spacemacs initialization."
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    ())))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-)
